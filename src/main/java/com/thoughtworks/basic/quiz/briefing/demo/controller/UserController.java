e38d1b8191e56e7a19db17ecb87e60f50fb1da8a
yangqian <743295483@qq.com>
2020-10-21 15:16:34 +0800

    [yangqian] feat: controller request body validation.

--- src/main/java/com/thoughtworks/basic/quiz/briefing/demo/controller/UserController.java
+++ src/main/java/com/thoughtworks/basic/quiz/briefing/demo/controller/UserController.java
@@ -4,13 +4,17 @@ import com.thoughtworks.basic.quiz.briefing.demo.dto.Education;
 import com.thoughtworks.basic.quiz.briefing.demo.dto.User;
 import com.thoughtworks.basic.quiz.briefing.demo.service.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Validated
 public class UserController {
 
     private final UserService userService;
@@ -26,20 +30,28 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable Long id) {
+    public User getUserById(
+            @NotNull(message = "user id can not be null")
+            @DecimalMin(value = "1", message = "user id muse be number and greater than 1")
+            @PathVariable Long id) {
         return userService.getUserById(id);
     }
 
     @PostMapping("/{id}/educations")
     @ResponseStatus(HttpStatus.CREATED)
     public Education createUserEducation(
+            @NotNull(message = "user id can not be null")
+            @DecimalMin(value = "1", message = "user id muse be number and greater than 1")
             @PathVariable long id,
-            @RequestBody Education education) {
+            @RequestBody @Valid Education education) {
         return userService.createEducation(id, education);
     }
 
     @GetMapping("/{id}/educations")
-    public List<Education> getUserEducations(@PathVariable long id) {
+    public List<Education> getUserEducations(
+            @NotNull(message = "user id can not be null")
+            @DecimalMin(value = "1", message = "user id muse be number and greater than 1")
+            @PathVariable long id) {
         return userService.getEducationsByUserId(id);
     }
 }

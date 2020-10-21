3b7241c9ef8effbe54dffd7776805834b98c3d03
yangqian <743295483@qq.com>
2020-10-21 11:19:57 +0800

    [yangqian] init: project.

---
+++ src/main/java/com/thoughtworks/basic/quiz/briefing/demo/BasicQuizBriefingApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.basic.quiz.briefing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicQuizBriefingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicQuizBriefingApplication.class, args);
+	}
+
+}

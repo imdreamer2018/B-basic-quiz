3e35d219189cdace6da416121938d9bdf823c571
yangqian <743295483@qq.com>
2020-10-21 12:39:33 +0800

    [yangqian] fix: user builder.

--- src/main/java/com/thoughtworks/basic/quiz/briefing/demo/dto/User.java
+++ src/main/java/com/thoughtworks/basic/quiz/briefing/demo/dto/User.java
@@ -1,12 +1,14 @@
 package com.thoughtworks.basic.quiz.briefing.demo.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
 
     private long id;

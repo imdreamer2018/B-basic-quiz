512fd7198a21be4b64f1896a38b21a1e3151dbe9
yangqian <743295483@qq.com>
2020-10-21 14:44:35 +0800

    [yangqian] feat: create user education and get user education service.

--- src/main/java/com/thoughtworks/basic/quiz/briefing/demo/service/UserService.java
+++ src/main/java/com/thoughtworks/basic/quiz/briefing/demo/service/UserService.java
@@ -1,10 +1,14 @@
 package com.thoughtworks.basic.quiz.briefing.demo.service;
 
+import com.thoughtworks.basic.quiz.briefing.demo.dto.Education;
 import com.thoughtworks.basic.quiz.briefing.demo.dto.User;
 import com.thoughtworks.basic.quiz.briefing.demo.exception.ResourceNotFoundException;
+import com.thoughtworks.basic.quiz.briefing.demo.repository.EducationRepository;
 import com.thoughtworks.basic.quiz.briefing.demo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -12,8 +16,11 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    private final EducationRepository educationRepository;
+
+    public UserService(UserRepository userRepository, EducationRepository educationRepository) {
         this.userRepository = userRepository;
+        this.educationRepository = educationRepository;
     }
 
     public User createUser(User userRequest) {
@@ -26,6 +33,26 @@ public class UserService {
     public User getUserById(long userId) {
         Optional<User> userOptional = userRepository.findById(userId);
         return userOptional.orElseThrow(() -> new ResourceNotFoundException(
-                "can not find basic info of user with id is" + userId));
+                "can not find basic info of user with id is " + userId));
+    }
+
+    public Education createEducation(long userId, Education education) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        User user = userOptional.orElseThrow(() -> new ResourceNotFoundException(
+                "can not find basic info of user with id is " + userId));
+        education.setId(educationRepository.getMaxEducationIdId());
+        education.setUserId(user.getId());
+        educationRepository.setMaxEducationId();
+        educationRepository.save(education);
+        return education;
+    }
+
+    public List<Education> getEducationsByUserId(long userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (!userOptional.isPresent()) {
+            throw new ResourceNotFoundException(
+                    "can not find basic info of user with id is " + userId);
+        }
+        return educationRepository.findByUserId(userId);
     }
 }

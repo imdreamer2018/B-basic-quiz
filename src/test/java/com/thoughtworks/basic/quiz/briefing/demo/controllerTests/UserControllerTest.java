5d0e1fc3498d045d17e51c5fd156520e81ede7c0
yangqian <743295483@qq.com>
2020-10-27 20:15:59 +0800

    [yangqian] fix: user controller test.

--- src/test/java/com/thoughtworks/basic/quiz/briefing/demo/controllerTests/UserControllerTest.java
+++ src/test/java/com/thoughtworks/basic/quiz/briefing/demo/controllerTests/UserControllerTest.java
@@ -1,35 +1,47 @@
 package com.thoughtworks.basic.quiz.briefing.demo.controllerTests;
 
+import com.thoughtworks.basic.quiz.briefing.demo.controller.UserController;
 import com.thoughtworks.basic.quiz.briefing.demo.dto.Education;
 import com.thoughtworks.basic.quiz.briefing.demo.dto.User;
-import com.thoughtworks.basic.quiz.briefing.demo.repository.EducationRepository;
-import com.thoughtworks.basic.quiz.briefing.demo.repository.UserRepository;
+import com.thoughtworks.basic.quiz.briefing.demo.exception.ResourceNotFoundException;
+import com.thoughtworks.basic.quiz.briefing.demo.service.UserService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(UserController.class)
+@AutoConfigureJsonTesters
 class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private UserRepository userRepository;
+    @MockBean
+    private UserService userService;
 
     @Autowired
-    private EducationRepository educationRepository;
+    private JacksonTester<User> userJson;
+
+    @Autowired
+    private JacksonTester<Education> educationJson;
 
     private User user;
 
@@ -51,61 +63,72 @@ class UserControllerTest {
                 .build();
     }
 
+    @AfterEach
+    void afterEach() {
+        Mockito.reset(userService);
+    }
+
     @Test
     void should_return_user_info_when_get_create_user_success() throws Exception {
-        String jsonValue =  "{\"name\":\"yangqian\", " +
-                "\"age\": 18, " +
-                "\"avatar\":\"https://inews.gtimg.com/newsapp_match/0/3581582328/0\", " +
-                "\"description\":\"description mock\"}";
+        when(userService.createUser(user)).thenReturn(user);
         mockMvc.perform(post("/users")
-                .content(jsonValue).contentType(MediaType.APPLICATION_JSON))
+                .content(userJson.write(user).getJson())
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.name", is("yangqian")));
+        verify(userService).createUser(user);
     }
 
     @Test
     void should_return_user_info_when_get_user_by_id() throws Exception {
-        userRepository.save(user);
-        mockMvc.perform(get("/users/1"))
+        when(userService.getUserById(1L)).thenReturn(user);
+        mockMvc.perform(get("/users/{id}", 1L))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.name", is("yangqian")));
+        verify(userService).getUserById(1L);
     }
 
     @Test
     void should_return_exception_when_get_user_by_id_not_found() throws Exception {
-        mockMvc.perform(get("/users/1"))
+        when(userService.getUserById(1L)).thenThrow(new ResourceNotFoundException("can not find basic info of user with id is 1"));
+        mockMvc.perform(get("/users/{id}", 1L))
                 .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.status", is(404)));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message", is("can not find basic info of user with id is 1")));
+        verify(userService).getUserById(1L);
     }
 
     @Test
     void should_return_education_info_when_create_user_education_by_user_id_success() throws Exception {
-        userRepository.save(user);
-        String jsonValue =  "{\"year\":2020, " +
-                "\"title\":\"mock education title\", " +
-                "\"description\":\"mock education description\"}";
-        mockMvc.perform(post("/users/1/educations")
-                .content(jsonValue).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.title", is("mock education title")));
+        when(userService.createEducation(1L, education)).thenReturn(education);
+        mockMvc.perform(post("/users/{id}/educations", 1L)
+                .content(educationJson.write(education).getJson())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+        verify(userService).createEducation(1L, education);
     }
 
     @Test
     void should_return_exception_when_create_user_education_by_user_id_not_found() throws Exception {
-        String jsonValue =  "{\"year\":2020, " +
-                "\"title\":\"mock education title\", " +
-                "\"description\":\"mock education description\"}";
-        mockMvc.perform(post("/users/1/educations")
-                .content(jsonValue).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
+        when(userService.createEducation(1L, education)).thenThrow(new ResourceNotFoundException(
+                "can not find basic info of user with id is 1"));
+        mockMvc.perform(post("/users/{id}/educations", 1L)
+                .content(educationJson.write(education).getJson())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message", is("can not find basic info of user with id is 1")));
+        verify(userService).createEducation(1L, education);
     }
 
     @Test
     void should_return_educations_when_get_user_educations_success() throws Exception {
-        userRepository.save(user);
-        educationRepository.save(education);
-        mockMvc.perform(get("/users/1/educations"))
+        List<Education> educations = new ArrayList<>();
+        educations.add(education);
+        when(userService.getEducationsByUserId(1L)).thenReturn(educations);
+        mockMvc.perform(get("/users/{id}/educations", 1L))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$[0].title", is("mock education title")));
     }
 }

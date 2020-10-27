66398e0b1b0c8c2d9af49e434f1761c846937719
yangqian <743295483@qq.com>
2020-10-27 20:15:43 +0800

    [yangqian] fix: remove CrudRepository and use JpaRepository.

--- src/main/java/com/thoughtworks/basic/quiz/briefing/demo/repository/EducationRepository.java
+++ src/main/java/com/thoughtworks/basic/quiz/briefing/demo/repository/EducationRepository.java
@@ -1,15 +1,14 @@
 package com.thoughtworks.basic.quiz.briefing.demo.repository;
 
 import com.thoughtworks.basic.quiz.briefing.demo.dto.Education;
-import com.thoughtworks.basic.quiz.briefing.demo.dto.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
-public interface EducationRepository extends CrudRepository<Education, Long> {
+public interface EducationRepository extends JpaRepository<Education, Long> {
 
     @Query(nativeQuery = true, value = "select * from Education e where e.user_id = :userId")
     @Transactional
